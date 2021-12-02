bd2fc9cd1381b3bddc0299be3dfa0c0747278a90
Joe Paris <josephparis6@gmail.com>
2021-12-02 13:40:03 -0700

    update:

---
+++ src/views/Cart.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  <h1>Your Cart</h1>
+  <ProductList :products="cart" />
+</div>
+<footer>
+  <a href="https://github.com/BYUCS260/grocery-store-joeparis6">https://github.com/BYUCS260/grocery-store-joeparis6</a>
+</footer>
+</template>
+
+<script>
+import ProductList from "../components/ProductList.vue"
+export default {
+  name: 'Browse',
+  components: {
+    ProductList
+  },
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+    cart() {
+      return this.$root.$data.cart;
+    }
+  },
+
+}
+</script>
